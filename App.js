61e636766df3aa1d303e9f1e0a41668884dfd76c
FluidSense <johanneskv96@gmail.com>
2018-10-02 17:53:32 +0200

    example on how to use AsyncStorage #9

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, AsyncStorage } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      asyncText: '',
+    }
+  }
+
+  componentDidMount() {
+    this.getTextFromAsyncStorage();
+  }
+
+  saveInput = async () => {
+    try {
+      await AsyncStorage.setItem('mykey_lebowski', this.state.text);
+    } catch (error) {
+      // Error saving data
+    }
+  }
+
+  getTextFromAsyncStorage = async () => {
+    try {
+      const value = await AsyncStorage.getItem('mykey_lebowski');
+      if (value !== null) {
+        // We have data!!
+        this.setState({asyncText: value});
+      }
+     } catch (error) {
+       // Error retrieving data
+       return "No text found yet"
+     }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <TextInput 
+          onChangeText={(text) => this.setState({text})}
+        />
+        <Button
+          onPress={() => {this.saveInput();this.getTextFromAsyncStorage();}}
+          title="Save input to AsyncStorage"
+          accessibilityLabel="Do you really need this?"
+        />
+        <Text>{this.state.asyncText}</Text>
       </View>
     );
   }
