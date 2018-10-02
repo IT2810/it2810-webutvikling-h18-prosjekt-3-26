139cfdb3cbad139308ed8f820fda6a25d5d4a501
Marius Johan Holm Johansen <mjjohans@stud.ntnu.no>
2018-10-02 12:56:33 +0200

    started working on routing and navigation on new branch

--- App.js
+++ App.js
@@ -1,21 +1,109 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import { createStackNavigator, StackActions, NavigationActions } from 'react-navigation'; // Version can be specified in package.json
 
-export default class App extends React.Component {
+class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'yellow', }}>
+        <Text>Welcome to the best website on the web.</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => {
+            this.props.navigation.dispatch(StackActions.reset({
+              index: 0,
+              actions: [
+                NavigationActions.navigate({ routeName: 'Details' })
+              ],
+            }))
+          }}
+        />
       </View>
     );
-  }
+  }  
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'blue', }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go to Home Screen"
+          onPress={() => {
+            this.props.navigation.dispatch(StackActions.reset({
+              index: 0,
+              actions: [
+                NavigationActions.navigate({ routeName: 'Home' })
+              ],
+            }))
+          }}
+        />
+        <Text> Yo you wanna see something funny</Text>
+        <Button
+          title="Go to Lolscreen"
+          onPress={() => {
+            this.props.navigation.dispatch(StackActions.reset({
+              index: 0,
+              actions: [
+                NavigationActions.navigate({ routeName: 'Lolscreen' })
+              ],
+            }))
+          }}
+        />
+      </View>
+    );
+  }  
+}   
+
+class Lolscreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'green', }}>
+        <Text>Lolscreen</Text>
+        <Button
+          title="Go to Home Screen"
+          onPress={() => {
+            this.props.navigation.dispatch(StackActions.reset({
+              index: 0,
+              actions: [
+                NavigationActions.navigate({ routeName: 'Home' })
+              ],
+            }))
+          }}
+        />
+        <Text>Back to details dog</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => {
+            this.props.navigation.dispatch(StackActions.reset({
+              index: 0,
+              actions: [
+                NavigationActions.navigate({ routeName: 'Details' })
+              ],
+            }))
+          }}
+        />
+      </View>
+    );  
+  }  
+}   
+
+
+const RootStack = createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  },
+  Details: {
+    screen: DetailsScreen
+  },
+  Lol: {
+    screen: Lolscreen
   },
 });
+
+export default class App extends React.Component{
+  render() {
+    return <RootStack />
+  }
+}
