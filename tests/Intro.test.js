279cd63a1d045155e2cce95c162541152e5ab0a9
FluidSense <johanneskv96@gmail.com>
2018-10-02 12:28:19 +0200

    remove faulty import in test

--- tests/Intro.test.js
+++ tests/Intro.test.js
@@ -1,10 +1,9 @@
 // __tests__/Intro-test.js
 import React from 'react';
-import Intro from '../Intro';
 
 import renderer from 'react-test-renderer';
 
 test('renders correctly', () => {
-  const tree = renderer.create(<Intro />).toJSON();
+  const tree = renderer.create(<View />).toJSON();
   expect(tree).toMatchSnapshot();
 });
