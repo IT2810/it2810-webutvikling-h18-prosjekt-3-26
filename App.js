b811c313502af88ef07672506d86ed9ebbe4bfd1
Marius Johan Holm Johansen <mjjohans@stud.ntnu.no>
2018-10-02 16:07:58 +0200

    #3 moved functionality into seperate files.

--- App.js
+++ App.js
@@ -1,73 +1,20 @@
 import React from 'react';
 import { View, Text, Button, StatusBar } from 'react-native';
-import { createStackNavigator, StackActions, NavigationActions } from 'react-navigation'; // Version can be specified in package.json
-import { Ionicons } from '@expo/vector-icons'; // 6.2.2
-import { TabNavigator, TabBarTop } from 'react-navigation'; // 1.0.0-beta.27
+import { TabNavigator, TabBarTop } from 'react-navigation';
+import { Ionicons } from '@expo/vector-icons';
+import HomeScreen from './src/screens/HomeScreen';
+import WorkoutScreen from './src/screens/WorkoutScreen';
+import SettingScreen from './src/screens/SettingScreen';
 
-
-class HomeScreen extends React.Component {
+class App extends React.Component {
   render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white', }}>
-      <StatusBar hidden />
-        <Text>Welcome to the best website on the web.</Text>
-        <Button
-          title="Go to My Workouts"
-          onPress={() => {
-            this.props.navigation.navigate('Workouts')
-          }}
-        />
+    return(
+      <View>
+        <HomeScreen />
       </View>
-    );
-  }  
+    )
+  }
 }
-
-class WorkoutScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'blue', }}>
-        <Text>Workouts Screen</Text>
-        <Button
-          title="Go to Home Screen"
-          onPress={() => {
-            this.props.navigation.navigate('Home')
-          }}
-        />
-        <Text> Yo you wanna see something funny</Text>
-        <Button
-          title="Go to Settings"
-          onPress={() => {
-            this.props.navigation.navigate('Settings')
-          }}
-        />
-      </View>
-    );
-  }  
-}   
-
-class SettingScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'green', }}>
-        <Text>Lolscreen</Text>
-        <Button
-          title="Go to Home Screen"
-          onPress={() => {
-            this.props.navigation.navigate('Home')
-          }}
-        />
-        <Text>Back to Workouts dog</Text>
-        <Button
-          title="Go to Workouts"
-          onPress={() => {
-            this.props.navigation.navigate('Workouts')
-          }} 
-        />
-      </View>
-    );  
-  }  
-}   
-
 
 export default TabNavigator(
   {
@@ -75,8 +22,6 @@ export default TabNavigator(
     Workouts: { screen: WorkoutScreen },
     Settings: { screen: SettingScreen
     }
-
-    
   },
   {
     navigationOptions: ({ navigation }) => ({
