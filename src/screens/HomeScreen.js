4ebdabac00cd47ef1e79da1bc141ffe15a721179
Marius Johan Holm Johansen <mjjohans@stud.ntnu.no>
2018-10-03 15:14:46 +0200

    #6 added dependencies for grid and created initial layout for homepage with placeholder.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,35 +1,53 @@
 import React from 'react';
-import { View, Text, Button, StatusBar, FlatList, StyleSheet } from 'react-native';
+import { View, Text, Button, StatusBar, FlatList, StyleSheet } from 'react-native'
+import { Col, Row, Grid } from "react-native-easy-grid";
+
 
 class HomeScreen extends React.Component {
     render() {
       return (
-        <View>
-        <StatusBar hidden />
-        <FlatList 
-          data={[{key: 'a'}, {key: 'b'}, {key: 'c'}]}
-          style={styles.container}
-          numColumns={3}
-          renderItem={({item}) => <Text>{item.key}</Text>} 
-          />
-        </View>
+          <Grid>
+            <Row size={15} style={{backgroundColor: 'green'}}>     
+              <Text style={{color:'white'}}> Next Workout goes here </Text>
+            </Row >
+            
+            <Row size={10} style={{backgroundColor:'black'}}>
+            </Row>
+
+            <Row size={17}>
+              <Col size={10} style={{backgroundColor: 'pink'}}>  
+              <Text>Notifications</Text>         
+              </Col>
+              <Col size={10} style={{backgroundColor: 'yellow'}}>
+              <Text>Calendar</Text>
+              </Col>
+              <Col size={10} style={{backgroundColor: 'grey'}}>
+              <Text>Something</Text>
+              </Col>
+            </Row>
+
+            <Row size={10}>
+
+            </Row>
+
+            <Row size={30}>
+              <Col size={10} style={{backgroundColor: 'black'}}>        
+              </Col>
+              <Col size={40} style={{backgroundColor: 'white'}}>
+                <Text style={{color:'black'}}>Pedometer</Text>
+              </Col>
+              <Col size={10} style={{backgroundColor: 'black'}}>
+              </Col>
+            </Row>
+          </Grid>
       );
     }  
   }
 
 const styles=StyleSheet.create({
-  container: {
-    backgroundColor: 'red',
-    flex: 1,
-    padding: 40,
+  gridstyle: {
+    color: 'yellow',
+    fontSize: 24,
   }
 })
   export default HomeScreen;
-
-            
-          /*<Button
-            title="Go to My Workouts"
-            onPress={() => {
-              this.props.navigation.navigate('Workouts')
-            }}
-          />*/
