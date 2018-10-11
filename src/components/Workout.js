4db88fb90877fb3420edaf75854b4624a4017b5d
FluidSense <johanneskv96@gmail.com>
2018-10-11 07:33:56 +0200

    begin work on async workout #7#9

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -1,3 +1,5 @@
+import { getFromStorage } from '../utils/asyncstorageUtils';
+import { AsyncStorage } from 'react-native';
 
 export default class Workout {
     constructor() {
@@ -16,12 +18,37 @@ export default class Workout {
     // Add exercise to this workout.
     addExercise = (exercise) => {
         this.exercises.push(exercise);
-        console.log(this.date.getDate());
     }
+
+    // Save workout on title+milliseconds since Jan 1, 1970. to prevent accidental overwriting.
+    // Also add key to list of workouts
+    save = async () => {
+        try {
+            console.log('pre-await')
+            await AsyncStorage.clear();
+            const workoutsInAsyncStorage = await getFromStorage('workouts');
+            const cardKey = this.title+this.date.getTime();
+            await AsyncStorage.setItem(cardKey, JSON.stringify(this));
+            //const logitem = await AsyncStorage.getFromStorage(cardKey);
+            //console.log(logitem);
+            workoutsInAsyncStorage = workoutsInAsyncStorage ? JSON.parse(workoutsInAsyncStorage) : [];
+            workoutsInAsyncStorage.push(cardKey);
+            await AsyncStorage.setItem('workouts',JSON.stringify(workoutsInAsyncStorage));
+        } catch (error) {
+            console.error('Could not save workout to local storage: ',error);
+        }
+    }
+    
+
 
     // Remove exercise from this workout
     removeExercise = (exercise) => {
         this.exercises.filter(a => a !== exercise);
+    }
+
+    edit = (paramObject) => {
+        'note' in paramObject ? this.note = paramObject.note : false;
+
     }
 
 }
