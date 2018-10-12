6027dbb3c772facf9269be99d0e171dad0d5196f
FluidSense <johanneskv96@gmail.com>
2018-10-12 13:57:11 +0200

    add method for removing workout in overview #7

--- src/components/WorkoutCard.js
+++ src/components/WorkoutCard.js
@@ -8,14 +8,22 @@ export default WorkoutCard = (props) => {
     Could as of now be extracted as a method in OverviewWorkoutScreen since it is so simple.
     */
 
+    const remove = () => {
+        console.log(JSON.stringify(props.workout));
+        //props.workout.delete();
+        props.removeWorkout(props.workout);        
+    }
+
+    //
 
     return (
         <React.Fragment>
-            <TouchableOpacity><Text>redcross</Text></TouchableOpacity>
+            <TouchableOpacity onPress={() => remove()} ><Text>redcross</Text></TouchableOpacity>
             <Text>{props.workout.title}</Text>
             <Text>{props.workout.date}</Text>
             <Text>{props.workout.note}</Text>
-            <Text>{props.workout.exercises}</Text>
+            {props.workout.exercises.map( (exercise, i) => <Text key={i}>{exercise.name}</Text>)}
+            
         </React.Fragment>
     );
 }
