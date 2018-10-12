73659e133b4fc7445a9a37f4b706ef0216bf0697
FluidSense <johanneskv96@gmail.com>
2018-10-12 13:56:11 +0200

    add delete to workout, change util to async #7 #9

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -7,6 +7,7 @@ export default class Workout {
         this.exercises = [];
         this.note = '';
         this.title = 'New Workout';
+        this.saveKey = 0;
     }
 
     // Allow user to set date on a workout. 
@@ -25,18 +26,33 @@ export default class Workout {
     save = async () => {
         try {
             console.log('pre-await')
-            await AsyncStorage.clear();
-            const workoutsInAsyncStorage = await getFromStorage('workouts');
-            const cardKey = this.title+this.date.getTime();
-            await AsyncStorage.setItem(cardKey, JSON.stringify(this));
+            //await AsyncStorage.clear();
+            const workoutsInAsyncStorage = await AsyncStorage.getItem('workouts');
+            this.saveKey = this.title+this.date.getTime();
+            await AsyncStorage.setItem(this.saveKey, JSON.stringify(this));
             //const logitem = await AsyncStorage.getFromStorage(cardKey);
             //console.log(logitem);
             workoutsInAsyncStorage = workoutsInAsyncStorage ? JSON.parse(workoutsInAsyncStorage) : [];
-            workoutsInAsyncStorage.push(cardKey);
+            workoutsInAsyncStorage.push(this.saveKey);
             await AsyncStorage.setItem('workouts',JSON.stringify(workoutsInAsyncStorage));
         } catch (error) {
             console.error('Could not save workout to local storage: ',error);
         }
+    }
+
+    delete = async () => {
+        let workoutsInAsyncStorage = await AsyncStorage.getItem('workouts');
+        workoutsInAsyncStorage = workoutsInAsyncStorage ? JSON.parse(workoutsInAsyncStorage) : [];
+        const workoutIndex = workoutsInAsyncStorage.indexOf(this.saveKey);
+        if (workoutIndex !==-1) {
+            await AsyncStorage.removeItem(this.saveKey, () => {
+                console.error('failed to remove '+this.saveKey+' from storage');
+                return;
+            });
+            workoutsInAsyncStorage.splice(workoutIndex,1);
+            await AsyncStorage.setItem('workouts',workoutsInAsyncStorage);
+        }
+
     }
     
 
