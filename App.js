a239454b1d0ad8e8cc2b8cbb18097014879bb65b
Peter Rydberg <peter.rydberg@hotmail.no>
2018-10-15 22:33:08 +0200

    #5 Started AsyncStorage for startDate

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Navigator} from './src/components/Navigator/CreateNavigator';
+import {Navigator, AsyncStorage} from './src/components/Navigator/CreateNavigator';
 
 export default class App extends React.Component {
 
@@ -7,12 +7,50 @@ export default class App extends React.Component {
     super(props);
 
     // States for global pedometer
-    this.state = { startDate: new Date(), pedActivated: false, globalStepCount: 0, prevGlobalStepCount: 0 };
+    this.state = { pedActivated: false, globalStepCount: 0, prevGlobalStepCount: 0 };
+
+    this.startDate = new Date();
 
     // Functions for updating states from global pedometer
     this.updateActivated = this.updateActivated.bind(this);
     this.updateGlobalSteps = this.updateGlobalSteps.bind(this);
     this.updatePrevGlobalSteps = this.updatePrevGlobalSteps.bind(this);
+  }
+
+  // Sets a startDate state for this session.
+  componentDidMount(){
+    this.startDate = this.retrieveStartDate();
+    console.log(this.startDate);
+  }
+
+  // Save start date locally, so pedometer can check the global number of steps from that date.
+  saveStartDate = async (startDate) => {
+    try {
+      await AsyncStorage.setItem('startDate', startDate);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  // Load start date.
+  retrieveStartDate = async () => {
+    let startDate = new Date();
+    try {
+      startDate = await AsyncStorage.getItem('startDate') || 'none';
+
+      console.log(startDate);
+
+      if(startDate === 'none'){
+        let newStartDate = new Date();
+        saveStartDate(newStartDate);
+        return newStartDate;
+      }
+      else{
+        return startDate;
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
   }
 
   // Updates availability
@@ -34,12 +72,12 @@ export default class App extends React.Component {
   render() {
     // For sending pedometer props to navigator
     var homeProps = {};
-    homeProps.startDate = this.state.startDate;
+    homeProps.startDate = this.startDate;
     homeProps.pedActivated = this.state.pedActivated;
     homeProps.globalStepCount = this.state.globalStepCount;
 
     var settingsProps = {};
-    settingsProps.startDate = this.state.startDate;
+    settingsProps.startDate = this.startDate;
     settingsProps.pedActivated = this.state.pedActivated;
     settingsProps.updateActivated = this.updateActivated;
     settingsProps.updateGlobalSteps = this.updateGlobalSteps;
