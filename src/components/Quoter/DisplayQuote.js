300d56909b7afe9b88ca496845e8b43bbd038111
Marius Johan Holm Johansen <mjjohans@stud.ntnu.no>
2018-10-16 14:25:38 +0200

    #6 Fixed displaying JSON correctly

--- src/components/Quoter/DisplayQuote.js
+++ src/components/Quoter/DisplayQuote.js
@@ -4,17 +4,17 @@ import { FlatList, View, Text } from 'react-native';
 
 const text = jsonquotes[Math.floor(Math.random()*jsonquotes.length)].text;
 const author = jsonquotes[Math.floor(Math.random()*jsonquotes.length)].from;
-const data = text + "-" + author;
+const dataz = text + " - " + author;
+
+console.log(dataz);
 
 class DisplayQuote extends React.Component{
     render() {
         return(
-            <View>
-            <FlatList 
-            data={data}
-            renderItem={({item})=><Text>{item}</Text>
-            }
-            />
+            <View style={{display:"flex",justifyContent:"center",}}>
+                <Text>
+                   {dataz}
+                </Text>
             </View>
         )
     }
