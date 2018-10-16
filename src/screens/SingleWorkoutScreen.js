edce937056e0963ee5b1590dad042fed17529e16
FluidSense <johanneskv96@gmail.com>
2018-10-16 21:31:57 +0200

    add method for adding exercise to workout #7

--- src/screens/SingleWorkoutScreen.js
+++ src/screens/SingleWorkoutScreen.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import { Button } from "react-native-paper";
 import ObjectEdit from '../components/EditObject';
+import Exercise from '../components/Exercise';
 
 
 export default class SingleWorkoutScreen extends React.Component {
     // This class takes an exercise as a prop and displays it nicely.
 
     createExercise = (workout) => {
-        const exercisesWithEdit = [];
-        for (const exerciseNr in workout.exercises) {
-            exercisesWithEdit.push(<ObjectEdit key={exerciseNr} propObject={workout.exercises[exerciseNr]} ints={['weight','repetitions']}/>);
-        }
+        const exercisesWithEdit = workout.exercises.map((exercise,i) => <ObjectEdit key={i} propObject={exercise} ints={['weight','repetitions']} labels/>);
         return exercisesWithEdit;
     }
 
@@ -24,14 +22,23 @@ export default class SingleWorkoutScreen extends React.Component {
         const exercises = this.createExercise(workout);
         if (workout) {
             return (
+            <ScrollView>
                 <View>
                     <Button mode="contained" compact onPress={() => this.props.navigation.goBack()}>Go back</Button>
-                    <ObjectEdit propObject={workout} exclude={['saveKey']}/>
+                    <ObjectEdit propObject={workout} exclude={['saveKey']} labels/>
                     <Text>{JSON.stringify(workout.date)}</Text>
                     <Text>Exercises:</Text>
                     {exercises || 'No exercises added'}
+                    <Button onPress={() => {
+                        workout.addExercise(new Exercise());
+                        this.forceUpdate();
+                        }
+                    }>
+                        Add workout
+                    </Button>
                     <Button icon="save" mode="contained" compact onPress={() => workout.save().then(()=>{this.props.navigation.navigate('Home',{workout:workout})})}>Save</Button>
                 </View>
+            </ScrollView>
             );
         }
         return false;
