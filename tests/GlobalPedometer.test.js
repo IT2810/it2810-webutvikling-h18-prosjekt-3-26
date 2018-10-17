100b2bbd908b103f68561a20da0aaa82e493ebd9
PeterRydberg <peter.rydberg@hotmail.no>
2018-10-17 17:48:08 +0200

    Started testing

---
+++ tests/GlobalPedometer.test.js
@@ -0,0 +1,23 @@
+// __tests__/GlobalPedometer.js
+import React from 'react';
+import GlobalPedometer from '../src/components/Pedometer/GlobalPedometer';
+
+import renderer from 'react-test-renderer';
+
+test('Renders correctly', () => {
+  const tree = renderer.create(<GlobalPedometer />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+/*
+test('Displays text if activated', () => {
+  const props = {
+    startDate: new Date(),
+    globalStepCount: 4,
+    pedActivated: true
+  }
+
+  const testNumber = renderer.create(<GlobalPedometer {...props} />).toJSON();
+
+  expect(testNumber.text()).to.contain('4');
+});*/
